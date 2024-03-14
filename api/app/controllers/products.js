3755d26ccfe6663b85febeb98c89051d58fb4e28
José Joaquín Romero Vargas <jjoaquin.romero@gmail.com>
2024-03-14 14:57:21 -0400

    Update products.js
    Se añadió el controlador para crear la categoría V1.1

--- api/app/controllers/products.js
+++ api/app/controllers/products.js
@@ -1,10 +1,12 @@
-const UsersModel = require('../models/products');
+const ProductsModel = require('../models/products');
+const CategoriesModel = require('../models/categories');
+
 const obtenerproductos = (req, res)=>{
     res.send('Ruta que obtiene productos')
 }
 
 const crearProducto = async (req, res)=>{
-   //try {
+   try {
         const {nombre, descripcion, marca, cantidad_disponible, precio, envio, iva, costo, id_vendedor, estado_producto, calificacion, images, eliminar, id_categoria} = req.body;    
         const user =await UsersModel.create({
             nombre,
@@ -30,4 +32,20 @@ const crearProducto = async (req, res)=>{
     }
 }
 
-module.exports = {obtenerproductos, crearProducto}
+
+const crearCategoria = async (req, res)=>{
+   try {
+        const {nombre, descripcion} = req.body;    
+        const user =await UsersModel.create({
+            nombre,
+            descripcion
+       });
+        res.status(200).json({messgae: 'Categoría creada exitosamente', categories});
+
+   }catch (e) {
+        res.status(500).json({error:e});
+        
+    }
+}
+
+module.exports = {obtenerproductos, crearProducto, crearCategoria}
