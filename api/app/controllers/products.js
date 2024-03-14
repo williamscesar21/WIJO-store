882309b187b6a481dd1fcc0fe5b8825d3d4c62df
José Joaquín Romero Vargas <jjoaquin.romero@gmail.com>
2024-03-14 14:48:39 -0400

    Update products.js
    Controlador del productos V1.0

---
+++ api/app/controllers/products.js
@@ -0,0 +1,33 @@
+const UsersModel = require('../models/products');
+const obtenerproductos = (req, res)=>{
+    res.send('Ruta que obtiene productos')
+}
+
+const crearProducto = async (req, res)=>{
+   //try {
+        const {nombre, descripcion, marca, cantidad_disponible, precio, envio, iva, costo, id_vendedor, estado_producto, calificacion, images, eliminar, id_categoria} = req.body;    
+        const user =await UsersModel.create({
+            nombre,
+            descripcion,
+            marca,
+            cantidad_disponible,
+            precio,
+            envio,
+            iva,
+            costo,
+            id_vendedor,
+            estado_producto,
+            calificacion,
+            images,
+            eliminar,
+            id_categoria
+       });
+        res.status(200).json({messgae: 'Producto creado exitosamente', products});
+
+   }catch (e) {
+        res.status(500).json({error:e});
+        
+    }
+}
+
+module.exports = {obtenerproductos, crearProducto}
