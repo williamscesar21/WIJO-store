eb68fa782afc23d78125a9f8e7d5379380705a73
José Joaquín Romero Vargas <jjoaquin.romero@gmail.com>
2024-03-14 21:37:08 -0400

    Update users.js
    Se añadieron las rutas para obtener usuarios por ID y actualizar usuario por ID

--- api/app/routes/users.js
+++ api/app/routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {obtenerusuarios, crearUsuario} = require('../controllers/users');
+const {obtenerusuarios, obtenerUsuarioId, crearUsuario, actualizarUsuarioId} = require('../controllers/users');
 
 router.post('/', crearUsuario);
 router.get('/', obtenerusuarios);
+router.get('/id',obtenerUsuarioId);
+router.patch('/id',actualizarUsuarioId);
 
 module.exports = router;
