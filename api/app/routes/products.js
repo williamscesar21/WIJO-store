a67ad57bc05a3f47dbbb9d673352273c3312eb7d
José Joaquín Romero Vargas <jjoaquin.romero@gmail.com>
2024-03-14 21:43:41 -0400

    Update products.js
    Se añadió la ruta para obtener productos por Id y actualizar productos por Id

--- api/app/routes/products.js
+++ api/app/routes/products.js
@@ -5,5 +5,8 @@ const {obtenerproductos, crearProducto} = require('../controllers/products');
 
 router.post('/', crearProducto);
 router.get('/', obtenerproductos);
+router.get('/id', obtenerProductosId);
+router.patch('/id', actualizarProductosId);
+
 
 module.exports = router;
