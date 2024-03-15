527ce43f38800ac18108e9fe04e86e9cf95bc9f5
José Joaquín Romero Vargas <jjoaquin.romero@gmail.com>
2024-03-14 22:05:36 -0400

    Update products.js
    Se añadieron las rutas de la categoría (obtener y actualizar)

--- api/app/routes/products.js
+++ api/app/routes/products.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const {obtenerproductos, crearProducto} = require('../controllers/products');
+const {obtenerproductos, crearProducto, obtenerProductosId, actualizarProductosId, crearCategoria, obtenerCategorias, obtenerCategoriaId, actualizarCategoriaId } = require('../controllers/products');
 
 router.post('/', crearProducto);
 router.get('/', obtenerproductos);
 router.get('/id', obtenerProductosId);
 router.patch('/id', actualizarProductosId);
+
+router.post('/', crearCategoria);
+router.get('/', obtenerCategorias);
+router.get('/id', obtenerCategoriaId);
+router.patch('/id', actualizarCategoriaId);
 
 
 module.exports = router;
