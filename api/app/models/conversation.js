fc9f0cb806c5f5202ec649527b140d3c5413d721
José Joaquín Romero Vargas <jjoaquin.romero@gmail.com>
2024-03-19 19:28:59 -0400

    Update conversation.js
    Se cambiaron los nombres de los modelos de conversación a comentarios

--- api/app/models/conversation.js
+++ api/app/models/conversation.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Definir modelo para registrar los mensajes que envía el comprador al vendedor
-const conversacionEnvioSchema = new Schema({
+const comentarioEnvioSchema = new Schema({
     id_usuario_mensaje: {//Este es el Id del usuario comprador que envía el mensaje
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         require: true
     },
-    mensaje: {// Mensaje que envía el usuario comprador
+    comentario: {// Comentario que envía el usuario comprador
         type: String,
         minLenght: 1,
         maxlength: 5000,
@@ -30,8 +30,8 @@ const conversacionEnvioSchema = new Schema({
 })
 
 //Definir el modelo donde se le responde al comprador por parte del vendedor
-const conversacionRespuestaSchema = new Schema({
-    respuesta: {// Mensaje en forma de respuesta que envía el vendedor al comprador
+const comentarioRespuestaSchema = new Schema({
+    respuesta: {// Respuesta al comentario del usuario por parte del vendedor
         type: String,
         require: true
     },
@@ -45,6 +45,6 @@ const conversacionRespuestaSchema = new Schema({
     versionKey: false 
 })
 
-module.exports = mongoose.model('ConversacionEnvio', ConversacionEnvioSchema); //Exportación del módulo Conversación Envío 
-module.exports = mongoose.model('ConversacionRespuesta', ConversacionRespuestaSchema); //Exportación del módulo Conversación Respuesta 
+module.exports = mongoose.model('ComentarioEnvio', ComentarioEnvioSchema); //Exportación del módulo Conversación Envío 
+module.exports = mongoose.model('ComentarioRespuesta', ComentarioRespuestaSchema); //Exportación del módulo Conversación Respuesta 
 
