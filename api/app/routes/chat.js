b0d8ee54423ef785389fbb7c597c62d664fb5430
José Joaquín Romero Vargas <jjoaquin.romero@gmail.com>
2024-03-23 13:09:59 -0400

    Update chat.js
    Se crearon las rutas para el obtener y crear el chat y la conversacion

--- api/app/routes/chat.js
+++ api/app/routes/chat.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
 
+const {crearChat, crearConversacion, obtenerChat, obtenerConversacionId} = require('../controllers/chat')
+
+router.post('/', crearChat);
+router.get('/', obtenerChat)
+
+router.post('/', crearConversacion);
+router.post('/id/id2/', obtenerConversacionId); //id: Id del usuario comprador 
+                                                //id2: id del usuario vendedor
+
+module.exports = router;
